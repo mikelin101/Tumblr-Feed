5a02112ecbd1512cc32b00a7e78a6e2aeb2d5905
Michael Lin <mili@yahoo-inc.com>
2017-01-25 10:50:26 -0800

    milestone 2

--- Tumblr/PhotosViewController.m
+++ Tumblr/PhotosViewController.m
@@ -10,6 +10,8 @@
 
 @interface PhotosViewController ()
 
+@property (strong, nonatomic) NSArray *posts;
+
 @end
 
 @implementation PhotosViewController
@@ -17,6 +19,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *apiKey = @"Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV";
+    NSString *urlString =
+    [@"https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=" stringByAppendingString:apiKey];
+    
+    NSURL *url = [NSURL URLWithString:urlString];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    
+    NSURLSession *session =
+    [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]
+                                  delegate:nil
+                             delegateQueue:[NSOperationQueue mainQueue]];
+    
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request
+                                            completionHandler:^(NSData * _Nullable data,
+                                                                NSURLResponse * _Nullable response,
+                                                                NSError * _Nullable error) {
+                                                if (!error) {
+                                                    NSError *jsonError = nil;
+                                                    NSDictionary *responseDictionary =
+                                                    [NSJSONSerialization JSONObjectWithData:data
+                                                                                    options:kNilOptions
+                                                                                      error:&jsonError];
+                                                    NSLog(@"Response: %@", responseDictionary);
+                                                    
+                                                    self.posts = responseDictionary[@"response"][@"posts"];
+                                                } else {
+                                                    NSLog(@"An error occurred: %@", error.description);
+                                                }
+                                            }];
+    [task resume];
 }
 
 - (void)didReceiveMemoryWarning {
